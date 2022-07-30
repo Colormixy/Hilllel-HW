872f0819fb179133b4eed1cd33fe2740e6e20d5e
Colormixy <nikitoskot3@gmail.com>
2022-07-30 11:52:19 +0200

    HomeWorkTwo with the formatting of the code

--- src/mykyta/kotelevets/HomeWorkTwo.java
+++ src/mykyta/kotelevets/HomeWorkTwo.java
@@ -1,22 +1,17 @@
 package mykyta.kotelevets;
 
 public class HomeWorkTwo {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         String str = "I hate Git";
         String str_2 = "LOL";
-
         int wordsAmount = wordsCount(str);
         System.out.println("How many words in the sentence? - " + wordsAmount);
-
-
         // вирішив не додавати сканер, тому що у завданні
         // не було сказано про нього, але можу переробити зі сканером
         boolean palindrome = isPalindrome(str_2);
         System.out.println("Is the word 'LOL' a palindrome? - " + palindrome);
-
-
     }
+
     public static int wordsCount(String str) {
         int value = str.split(" ").length;
         return value;
@@ -24,11 +19,8 @@ public class HomeWorkTwo {
 
     public static boolean isPalindrome(String str_2) {
         int left = 0, right = str_2.length() - 1;
-
-        while(left < right)
-        {
-            if(str_2.charAt(left) != str_2.charAt(right))
-            {
+        while (left < right) {
+            if (str_2.charAt(left) != str_2.charAt(right)) {
                 return false;
             }
             left++;
