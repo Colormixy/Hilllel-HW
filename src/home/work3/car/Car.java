d99ad40dc72c1f2b405aded81cc7c7e56cf71663
Colormixy <nikitoskot3@gmail.com>
2022-08-12 13:04:40 +0200

    HW3 part 2

---
+++ src/home/work3/car/Car.java
@@ -0,0 +1,18 @@
+package home.work3.car;
+
+public class Car {
+    public void start() {
+        startElictricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startElictricity() {
+    }
+}
